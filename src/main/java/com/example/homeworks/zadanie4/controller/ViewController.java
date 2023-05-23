c0f419f6124016ea663014f8d7a54ad7b81e837c
KozlowskiKamil <kamil2k8@gmail.com>
2023-05-23 06:10:34 +0200

    nie działa do poprawy

--- src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
+++ src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,25 +20,27 @@ public class ViewController {
     public ViewController(ToolkitRepository toolkitRepository) {
         this.toolkitRepository = toolkitRepository;
     }
-
     @GetMapping("/")
-    public String getTools(Model model) {
-        List<Tool> tools = toolkitRepository.getTools();
-        model.addAttribute("tools", tools);
+    public String index() {
         return "index";
     }
 
-    @PostMapping("/")
+    @GetMapping("/read")
+    public String getTools(Model model) {
+        List<Tool> tools = toolkitRepository.getTools();
+        model.addAttribute("tools", tools);
+        return "read";
+    }
+
+    @PostMapping("/create")
     public String add(@RequestParam String name, @RequestParam float size, @RequestParam String unit,
                       @RequestParam("actions") List<String> actions, Model model) {
         Tool newTool = new Tool(name, new Tool.ToolSize(size, unit), actions);
         toolkitRepository.add(newTool);
         List<Tool> tools = toolkitRepository.getTools();
         model.addAttribute("tools", tools);
-        return "index";
+        return "create";
     }
-
-
 
 
 }
