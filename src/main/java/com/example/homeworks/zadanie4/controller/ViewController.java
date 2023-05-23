79737e8fb66d38eebce88bdcdfefab76dc77ea86
KozlowskiKamil <kamil2k8@gmail.com>
2023-05-23 04:30:13 +0200

    Dodanie narzędzia

--- src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
+++ src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
@@ -2,9 +2,14 @@ package com.example.homeworks.zadanie4.controller;
 
 import com.example.homeworks.zadanie4.model.Tool;
 import com.example.homeworks.zadanie4.service.ToolkitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -12,6 +17,7 @@ public class ViewController {
 
     private final ToolkitRepository toolkitRepository;
 
+    @Autowired // Dodaję aby było wiadomo gdzie jest DI choć wiadomo, że nie jest wymagana przy konstruktorze
     public ViewController(ToolkitRepository toolkitRepository) {
         this.toolkitRepository = toolkitRepository;
     }
@@ -22,5 +28,18 @@ public class ViewController {
         model.addAttribute("tools", tools);
         return "index";
     }
+
+    @PostMapping("/")
+    public String add(@RequestParam String name, @RequestParam float size, @RequestParam String unit,
+                      @RequestParam("actions") List<String> actions, Model model) {
+        Tool newTool = new Tool(name, new Tool.ToolSize(size, unit), actions);
+        toolkitRepository.add(newTool);
+        List<Tool> tools = toolkitRepository.getTools();
+        model.addAttribute("tools", tools);
+        return "index";
+    }
+
+
+
 
 }
