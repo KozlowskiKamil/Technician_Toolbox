05f4a3418cb232ccc0de3be48dc2216197dca378
KozlowskiKamil <kamil2k8@gmail.com>
2023-05-25 08:28:15 +0200

    update działą

--- src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
+++ src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
@@ -73,4 +73,17 @@ public class ViewController {
         return "search";
     }
 
+
+    @PostMapping("/updateTool")
+    public String updateTool(@RequestParam("id") Long id, @RequestParam("name") String name,
+                             @RequestParam("size") float size, @RequestParam("unit") String unit,
+                             @RequestParam("actions") List<String> actions) {
+        Tool existingTool = toolService.findById(id);
+        existingTool.setName(name);
+        existingTool.setToolSize(new Tool.ToolSize(size, unit));
+        existingTool.setActions(actions);
+        toolService.saveToolkit();
+        return "redirect:/read";
+    }
+
 }
