b19e1519c81d4ae253b61536e7162835508e7583
KozlowskiKamil <kamil2k8@gmail.com>
2023-06-02 15:39:42 +0200

    dodanie img do narzędzi v2

--- src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
+++ src/main/java/com/example/homeworks/zadanie4/controller/ViewController.java
@@ -37,7 +37,7 @@ public class ViewController {
 
     @PostMapping("/create")
     public String add(@RequestParam String name, @RequestParam float size, @RequestParam String unit, @RequestParam("actions") List<String> actions, Model model) {
-        Tool newTool = new Tool(name, new Tool.ToolSize(size, unit), actions, Path.of("/img/noImg.png"));
+        Tool newTool = new Tool(name, new Tool.ToolSize(size, unit), actions, Path.of("/img/noImg.png")); // TODO: 02.06.2023 Dodać metodę pobierania img od usera
         toolService.add(newTool);
         List<Tool> tools = toolService.getTools();
         model.addAttribute("tools", tools);
@@ -64,7 +64,7 @@ public class ViewController {
         Long idL = Long.valueOf(id);
         toolService.delete(idL);
         Tool.ToolSize toolSize = new Tool.ToolSize(toolDto.getSize(), toolDto.getUnit());
-        Tool tool = new Tool(toolDto.getName(), toolSize, actions, Path.of("/img/noImg.png"));
+        Tool tool = new Tool(toolDto.getName(), toolSize, actions, toolDto.getImg());
         toolService.add(tool);
         return "redirect:/read";
     }
