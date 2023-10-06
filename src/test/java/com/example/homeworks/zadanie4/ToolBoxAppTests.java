10df3b288bb801dedf529370196040f2f70cbc79
KozlowskiKamil <kamil2k8@gmail.com>
2023-10-06 23:45:04 +0200

    unit test

--- src/test/java/com/example/homeworks/zadanie4/ToolBoxAppTests.java
+++ src/test/java/com/example/homeworks/zadanie4/ToolBoxAppTests.java
@@ -1,10 +1,39 @@
 package com.example.homeworks.zadanie4;
 
+import com.example.homeworks.zadanie4.model.Tool;
+import com.example.homeworks.zadanie4.service.ToolService;
+import com.example.homeworks.zadanie4.service.ToolkitRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ToolBoxAppTests {
 
+    @Test
+    public void testAddTool() {
+        ToolService toolService = new ToolkitRepository();
+        Tool newTool = new Tool("Test Tool", "Test Location", new Tool.ToolSize(1.0f, "Test Unit"), List.of("Test Action"), Path.of("/img/test.png"));
+        boolean result = toolService.add(newTool);
+        assertTrue(result);
+        List<Tool> tools = toolService.getTools();
+        Tool addedTool = tools.get(tools.size() - 1);
+        assertEquals(newTool, addedTool);
+    }
 
-
+    @Test
+    public void testDeleteTool() {
+        ToolService toolService = new ToolkitRepository();
+        Tool newTool = new Tool("Test Tool", "Test Location", new Tool.ToolSize(1.0f, "Test Unit"), List.of("Test Action"), Path.of("/img/test.png"));
+        toolService.add(newTool);
+        Long idToDelete = newTool.getId();
+        toolService.delete(idToDelete);
+        List<Tool> tools = toolService.getTools();
+        Tool deletedTool = tools.stream().filter(t -> t.getId().equals(idToDelete)).findFirst().orElse(null);
+        assertNull(deletedTool);
+    }
 }
