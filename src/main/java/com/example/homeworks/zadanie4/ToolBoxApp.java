b0bc324b469c5f9b87e1ba698a149300a8aaa509
KozlowskiKamil <kamil2k8@gmail.com>
2023-10-06 21:56:28 +0200

    change view

--- src/main/java/com/example/homeworks/zadanie4/ToolBoxApp.java
+++ src/main/java/com/example/homeworks/zadanie4/ToolBoxApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Zadanie4Application {
+public class ToolBoxApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Zadanie4Application.class, args);
+        SpringApplication.run(ToolBoxApp.class, args);
     }
 
 }
--- src/test/java/com/example/homeworks/zadanie4/ToolBoxAppTests.java
+++ src/test/java/com/example/homeworks/zadanie4/ToolBoxAppTests.java
@@ -3,7 +3,7 @@ package com.example.homeworks.zadanie4;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Zadanie4ApplicationTests {
+class ToolBoxAppTests {
 
 
 
